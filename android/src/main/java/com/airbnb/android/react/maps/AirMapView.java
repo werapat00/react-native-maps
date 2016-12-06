92b876664a58830b09bfc091a8006221d3ec1baf
Werapat <werapat@gmail.com>
2016-12-06 15:37:17 +0700

    - Give priority to scrollview

--- android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
+++ android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
@@ -571,19 +571,20 @@ public class AirMapView extends MapView implements GoogleMap.InfoWindowAdapter,
 
         switch (action) {
             case (MotionEvent.ACTION_DOWN):
-                this.getParent().requestDisallowInterceptTouchEvent(true);
+                //this.getParent().requestDisallowInterceptTouchEvent(true);
                 isTouchDown = true;
                 break;
             case (MotionEvent.ACTION_MOVE):
                 startMonitoringRegion();
                 break;
             case (MotionEvent.ACTION_UP):
-                this.getParent().requestDisallowInterceptTouchEvent(false);
+                //this.getParent().requestDisallowInterceptTouchEvent(false);
                 isTouchDown = false;
                 break;
         }
-        super.dispatchTouchEvent(ev);
-        return true;
+        return super.dispatchTouchEvent(ev);
+        //super.dispatchTouchEvent(ev);
+        //return true;
     }
 
     // Timer Implementation
